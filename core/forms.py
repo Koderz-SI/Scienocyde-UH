d72ec7113988fd7e5be3a1e7364b86babc2d812b
Koderz-SI <78015033+Koderz-SI@users.noreply.github.com>
2021-03-16 12:25:29 +0530

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 
 class HostForm(forms.Form):
-	full_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':'Your Full Name'}))
-	email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control','placeholder':'name@example.com'}))
-	org = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
-	type_org = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
-	designation = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
-	phone = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control','placeholder':''}))
-	purpose = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder':''}))
-	detail = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder':''}))
-	theme = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
-	guidelines = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder':''}))
-	elig_cri = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
+	Your_Full_Name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':'Your Full Name'}))
+	Email_address = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control','placeholder':'name@example.com'}))
+	Organization_hosting_the_contest = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
+	Type_of_Organization = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
+	Designation = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
+	Phone_no = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control','placeholder':''}))
+	Purpose_of_Contest = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder':''}))
+	Breif_detail_about_your_event = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder':''}))
+	Theme_of_Project = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
+	Guidelines_for_submission = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder':''}))
+	Eligibility_Creteria = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':''}))
 	
