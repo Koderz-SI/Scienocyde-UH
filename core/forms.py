c808d1520c6ed07a991e943369e811306c9f970f
Koderz-SI <78015033+Koderz-SI@users.noreply.github.com>
2021-03-20 23:03:26 +0530

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -12,6 +12,11 @@ ef= [
     ('below 18', 'Below 12'),
     ]
 
+g = [
+    ('female', 'Female'),
+    ('male', 'Male'),
+    ]    
+
 
 class HostForm(forms.Form):
 	Your_Full_Name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control inp','placeholder':'Your Full Name'}))
@@ -25,4 +30,30 @@ class HostForm(forms.Form):
 	Theme_of_Project = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control inp','placeholder':''}))
 	Guidelines_for_submission = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control inp','placeholder':''}))
 	Eligibility_Creteria = forms.CharField(widget=forms.Select(choices=ef,attrs={'class': 'form-control inp','placeholder':''}))
+
+
+class ParticipantForm(forms.Form):
+	School_Name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control inp','placeholder':'Enter your school name'}))
+	School_Phone_no = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control inp','placeholder':'Enter your school phone no.'}))
+	School_Email_address = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control inp','placeholder':'name@example.com'}))
+	School_Address= forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control inp','placeholder':''}))
+	State = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control inp','placeholder':'Enter your school address'}))
+	Student_Name_1 = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control inp','placeholder':'Enter your name'}))
+	Contact_no = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control inp','placeholder':'Enter your phone no.'}))
+	Email_address = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control inp','placeholder':'name@example.com'}))
+	House_Address= forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control inp','placeholder':''}))
+	Gender= forms.CharField(widget=forms.Select(choices=g,attrs={'class': 'form-control inp','placeholder':''}))
+	Student_Name_2 = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control inp','placeholder':'Enter your name'}))
+	Contact_no = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control inp','placeholder':'Enter your phone no.'}))
+	Email_address = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control inp','placeholder':'name@example.com'}))
+	House_Address= forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control inp','placeholder':''}))
+	Gender= forms.CharField(widget=forms.Select(choices=g,attrs={'class': 'form-control inp','placeholder':''}))
+	Title_of_your_project= forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control inp','placeholder':''}))
+	Question_or_Problem= forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control inp','placeholder':''}))
+	Hypothesis_or_possible_solution = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control inp','placeholder':''}))
+	Materials_needed = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control inp','placeholder':''}))
+	Results = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control inp','placeholder':''}))
+	Image_of_Project= forms.CharField(widget=forms.ClearableFileInput(attrs={'class': 'form-control inp','placeholder':''}))
+	Link_of_your_project= forms.CharField(widget=forms.URLField(attrs={'class': 'form-control inp','placeholder':''}))
 	
+
